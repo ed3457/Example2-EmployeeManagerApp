9befea2b595616b0dc0eda514c8d4d4d6af1e10b
Kamel R <kamel.r@wayne.edu>
2023-09-18 17:58:49 -0400

    reused the code from the base class

--- Manager.cpp
+++ Manager.cpp
@@ -18,10 +18,8 @@ Manager::Manager(string s, string n, string dn, float b):Employee(s,n,dn)
 
 void Manager::printInfo()
 {
-	cout << "Manager Name:" << getName() << endl;
-	cout << "Manager Dept:" << getDeptName() << endl;
-	cout << "Manager SSN:" << getSSN() << endl;
-	cout << "Manager Bonus" << getBonus() << endl;
+	Employee::printInfo();
+	cout << "Employee Bonus" << getBonus() << endl;
 
 
 }
